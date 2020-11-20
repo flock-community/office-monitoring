c54275c60850a32f8f28a2903dca8ff0870e4fea
jvandis <jvandis@bol.com>
2020-11-20 16:51:10 +0100

    Add CORS

--- office-backend/src/main/kotlin/flock/community/office/monitoring/backend/controller/RestStreamController.kt
+++ office-backend/src/main/kotlin/flock/community/office/monitoring/backend/controller/RestStreamController.kt
@@ -5,11 +5,13 @@ import flock.community.office.monitoring.backend.UpdatesModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.ZonedDateTime
 
+@CrossOrigin(origins = ["https://office.flock.community.com"], maxAge = 3600)
 @RestController
 @RequestMapping(path = ["/device-updates"])
 @ExperimentalCoroutinesApi
