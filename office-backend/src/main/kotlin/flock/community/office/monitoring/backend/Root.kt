1fd5f3c9d471ea51f1371f1c13d09d079e027278
jvandis <jvandis@bol.com>
2020-11-24 21:04:55 +0100

    try out things with google app engine

--- office-backend/src/main/kotlin/flock/community/office/monitoring/backend/Root.kt
+++ office-backend/src/main/kotlin/flock/community/office/monitoring/backend/Root.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 class Root {
 
     @GetMapping
