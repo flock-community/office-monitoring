bd3051a9b044fad3aaf8db32ca5b3b2eaa4c30e5
Julius van Dis <jvandis@bol.com>
2020-12-04 12:42:14 +0100

    Substitute env variables build time for websocket host, using environment variable

--- frontend/src/RSocketUtil.js
+++ frontend/src/RSocketUtil.js
@@ -25,9 +25,7 @@ export const createRSocketClient = () => {
             metadataMimeType: 'message/x.rsocket.routing.v0',
         },
         transport: new RSocketWebSocketClient({
-            // url: `wss://flock-office-290609.ew.r.appspot.com/ws`
-            // url: `ws://localhost:9000/ws`
-            url: `ws://34.91.246.132:9000/ws`
+            url: `ws://${process.env.HOST}/ws`
         }),
     });
 };
