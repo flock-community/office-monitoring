22f36e2f6ab2dd24b919ef88c9c5fe054de47bba
Julius van Dis <jvandis@bol.com>
2020-12-18 13:37:25 +0100

    Properly merge starting message

--- office-backend/src/main/kotlin/flock/community/office/monitoring/backend/controller/StreamRoot.kt
+++ office-backend/src/main/kotlin/flock/community/office/monitoring/backend/controller/StreamRoot.kt
@@ -3,10 +3,7 @@ package flock.community.office.monitoring.backend.controller
 import flock.community.office.monitoring.backend.DeviceMessageWrapperDTO
 import flock.community.office.monitoring.backend.UpdatesModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.*
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.stereotype.Controller
@@ -28,9 +25,11 @@ internal class StreamRoot(private val updatesModel: UpdatesModel) {
     @MessageMapping("start")
     internal fun start(): Flow<DeviceMessageWrapperDTO> {
         log.info("Receiving")
-        return updatesModel.state
-            .onStart { starterMessage }
-            .onEach { log.info("Sending to client: $it") }
+        return listOf(starterMessage,
+            updatesModel.state
+                .onStart { starterMessage }
+                .onEach { log.info("Sending to client: $it") }
+        ).merge()
 
 //        Flux.just(DeviceMessageWrapperDTO("topic", ZonedDateTime.now(), "very first message"))
 //                .concatWith {
