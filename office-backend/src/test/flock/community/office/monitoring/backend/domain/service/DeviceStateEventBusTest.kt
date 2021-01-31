d080086a52ea0187b315a32e85ef834ad22cb4d8
Florian Putker <fputker@bol.com>
2021-01-31 09:19:15 +0100

    add two unit tests

--- office-backend/src/test/flock/community/office/monitoring/backend/domain/service/DeviceStateEventBusTest.kt
+++ office-backend/src/test/flock/community/office/monitoring/backend/domain/service/DeviceStateEventBusTest.kt
@@ -13,7 +13,9 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 import java.util.*
+import java.util.stream.IntStream.range
 import kotlin.random.Random
 
 @SpringBootTest
@@ -25,25 +27,40 @@ internal class DeviceStateEventBusTest() {
     @Autowired
     lateinit var objectMapper: ObjectMapper
 
-    @Test
-    fun `test when publishing messages to eventBus subscribe retrieves them`() = runBlocking {
+    fun createTestMessages(amount: Int): List<DeviceStateEntity>{
         val contactSensorStateBody = ContactSensorStateBody(
                 lastSeen = Instant.parse("2021-01-29T10:00:00.00Z"),
                 battery = Random.nextInt(0, 100),
                 voltage = Random.nextInt(0, 100),
                 contact = Random.nextBoolean())
 
-//        Wat is het verschil tussen deviceId en topic?
-        val testEntity = DeviceStateEntity(
-                UUID.randomUUID().toString(),
-                DeviceType.CONTACT_SENSOR,
-                "zigbee2mqtt/0x00158d000578385c",
-                Instant.now(),
-                objectMapper.writeValueAsString(contactSensorStateBody)
-        )
+        val list: MutableList<DeviceStateEntity> = mutableListOf()
+        for (i in range(0, amount)){
+            val dse = DeviceStateEntity(UUID.randomUUID().toString(),
+                    DeviceType.CONTACT_SENSOR,
+                    "zigbee2mqtt/0x00158d000578385c",
+                    Instant.now().minus(i.toLong(), ChronoUnit.MINUTES),
+                    objectMapper.writeValueAsString(contactSensorStateBody))
+            list.add(dse)
+        }
+        return list
+    }
 
+    @Test
+    fun `test when publishing a message to eventBus subscribe retrieves it`() = runBlocking {
+        val testEntity = createTestMessages(1)[0]
         testBus.publish(testEntity)
 
         assertEquals(testEntity, testBus.subscribe(null).first())
+    }
+
+    @Test
+    fun `test when publishing multiple messages to eventBus subscribe retrieves the last one`() = runBlocking {
+        val testEntities = createTestMessages(2)
+        testBus.publish(testEntities[0])
+
+        testBus.publish(testEntities[1])
+
+        assertEquals(testEntities[1], testBus.subscribe(null).first())
     }
 }
