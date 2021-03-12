a66761eb3ebb226b9a2c0976b79e07e6aa93dff2
Niels Simonides <niels.simonides@ing.com>
2021-03-12 13:27:39 +0100

    Loop trough all devices and create TimelineChartRecords for all states of these devices

--- frontend/src/routes/_AllDevicesHistoryChart.svelte
+++ frontend/src/routes/_AllDevicesHistoryChart.svelte
@@ -1,50 +1,45 @@
 <script lang="ts">
-  import { totalmem } from "os";
-
-  import {
-    ContactSensorState,
-    DeviceState,
-    DeviceType,
-    TimelineChartRecord,
-  } from "./model";
-
+  import { devicesStore, deviceStateStore } from "../services/stores";
+  import type { ContactSensorState, DeviceState } from "../services/StreamDtos";
+  import { DeviceType } from "../services/StreamDtos";
+  import type { TimelineChartRecord } from "./model";
   import DeviceHistoryChart from "./_DeviceHistoryChart.svelte";
-  import { groupBy } from "./_utils";
 
-  const now = new Date();
-  const before = new Date();
-  before.setHours(before.getHours() - 2);
+  const convertToChartData = (
+    deviceStates: DeviceState<ContactSensorState>[]
+  ) => {
+    return deviceStates
+      .map((state: DeviceState<ContactSensorState>) => {
+        if (state.body.contact === false) {
+          let openedOnDate = state.date;
 
-  const states: DeviceState[] = [
-    {
-      id: "1",
-      type: DeviceType.CONTACT_SENSOR,
-      deviceId: "door-1",
-      date: before,
-      body: {
-        lastSeen: now,
-        battery: 1,
-        voltage: 1,
-        contact: false,
-      },
-    },
-    {
-      id: "2",
-      type: DeviceType.CONTACT_SENSOR,
-      deviceId: "door-1",
-      date: now,
-      body: {
-        lastSeen: now,
-        battery: 1,
-        voltage: 1,
-        contact: true,
-      },
-    },
-  ];
+          // The states are per device so the next one must be the close state, if it's not found the sensor is still open
+          let closedOnDate =
+            deviceStates.find((s) => s.date > state.date && s.body.contact)
+              ?.date || new Date();
 
-  const groupedPerDevice = groupBy(states, (state) => state.deviceId);
+          let record: TimelineChartRecord = {
+            category: state.deviceId,
+            start: openedOnDate,
+            end: closedOnDate,
+          };
 
-  const chartData: TimelineChartRecord[] = [];
+          return record;
+        }
+      })
+      .filter((s) => s !== undefined);
+  };
+
+  const chartData = $devicesStore
+    .filter((device) => device.type === DeviceType.CONTACT_SENSOR)
+    .flatMap((contactSensor) => {
+      const contactSensorStates = $deviceStateStore.filter(
+        (state) => state.deviceId === contactSensor.id
+      );
+      const contactSensorStatesTyped = contactSensorStates as DeviceState<ContactSensorState>[];
+      return convertToChartData(contactSensorStatesTyped);
+    })
+    .filter((chartData) => chartData !== undefined);
 </script>
 
 <DeviceHistoryChart {chartData} />
--- frontend/src/services/StreamDtos.ts
+++ frontend/src/services/StreamDtos.ts
@@ -1,5 +1,26 @@
 export interface DeviceDto {
-    id: String;
-    name: String;
-    type: String;
+  id: String;
+  name: String;
+  type: DeviceType;
+}
+
+export interface DeviceState<T extends StateBody> {
+  id: String;
+  type: DeviceType;
+  deviceId: String;
+  date: Date;
+  body: T;
+}
+
+export enum DeviceType {
+  CONTACT_SENSOR,
+}
+
+export interface StateBody {}
+
+export interface ContactSensorState extends StateBody {
+  lastSeen: Date;
+  battery: Number;
+  voltage: Number;
+  contact: Boolean;
 }
