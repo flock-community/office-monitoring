07a0bb20bbddd78e312aca9ab872c63bda04e03b
Julius van Dis <vandis.j@gmail.com>
2021-04-23 15:35:37 +0200

    Use UUID's in the frontend

--- office-backend/src/main/kotlin/flock/community/office/monitoring/backend/domain/service/DevicesCommandExecutor.kt
+++ office-backend/src/main/kotlin/flock/community/office/monitoring/backend/domain/service/DevicesCommandExecutor.kt
@@ -6,25 +6,23 @@ import flock.community.office.monitoring.backend.controller.FlockMonitorCommandB
 import flock.community.office.monitoring.backend.controller.FlockMonitorCommandBody.GetDevicesCommand
 import flock.community.office.monitoring.backend.controller.FlockMonitorMessageBody.*
 import flock.community.office.monitoring.backend.controller.FlockMonitorMessageType.*
-import flock.community.office.monitoring.backend.domain.repository.mapping.DeviceStateMapper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import org.springframework.stereotype.Service
 
-interface Executor<T: FlockMonitorCommandBody> {
+interface Executor<T : FlockMonitorCommandBody> {
     fun getFlow(command: T): Flow<FlockMonitorMessage>
 
 }
 
 @Service
-class DevicesCommandExecutor: Executor<GetDevicesCommand>{
+class DevicesCommandExecutor : Executor<GetDevicesCommand> {
 
     override fun getFlow(command: GetDevicesCommand): Flow<FlockMonitorMessage> = flow {
 
-        val devices = devicesMappingConfigurations.map { (id, config) ->
-            Device(id, config.description, config.deviceType)
+        val devices = devicesMappingConfigurations.map { (sensorId, config) ->
+            Device(config.deviceId, config.description, config.deviceType, sensorId)
         }
 
         emit(FlockMonitorMessage(DEVICE_LIST_MESSAGE, DeviceListMessage(devices)))
@@ -32,17 +30,25 @@ class DevicesCommandExecutor: Executor<GetDevicesCommand>{
 }
 
 @Service
-class DevicesFeedCommandExecutor(val deviceStateHistoryService: DeviceStateHistoryService,
-                                 val deviceStateEventBus: DeviceStateEventBus): Executor<GetDeviceStateCommand> {
+class DevicesFeedCommandExecutor(
+    val deviceStateHistoryService: DeviceStateHistoryService,
+    val deviceStateEventBus: DeviceStateEventBus
+) : Executor<GetDeviceStateCommand> {
 
-    override fun getFlow(command: GetDeviceStateCommand ): Flow<FlockMonitorMessage> = flow {
+    override fun getFlow(command: GetDeviceStateCommand): Flow<FlockMonitorMessage> = flow {
 
-        deviceStateHistoryService.getHistory(command.deviceId, command.from).collect {
-            emit(FlockMonitorMessage(DEVICE_STATE, DeviceStateMessage(it)))
-        }
+        val mqttDeviceId = devicesMappingConfigurations.entries
+            .firstOrNull { it.value.deviceId == command.deviceId }
+            ?.key
 
-        deviceStateEventBus.subscribe(command.deviceId).collect {
-            emit(FlockMonitorMessage(DEVICE_STATE, DeviceStateMessage(it)))
+        if (mqttDeviceId != null) {
+            deviceStateHistoryService.getHistory(mqttDeviceId, command.from).collect {
+                emit(FlockMonitorMessage(DEVICE_STATE, DeviceStateMessage(it)))
+            }
+
+            deviceStateEventBus.subscribe(mqttDeviceId).collect {
+                emit(FlockMonitorMessage(DEVICE_STATE, DeviceStateMessage(it)))
+            }
         }
     }
 }
