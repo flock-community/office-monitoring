4fda905066eda55cfe95c76a2960569439e3dab9
Jacky To <cyto1984@gmail.com>
2021-04-23 15:36:25 +0200

    Update switch controller and publisher with off and state methods

--- office-backend/src/main/kotlin/flock/community/office/monitoring/backend/controller/SwitchController.kt
+++ office-backend/src/main/kotlin/flock/community/office/monitoring/backend/controller/SwitchController.kt
@@ -2,19 +2,32 @@ package flock.community.office.monitoring.backend.controller
 
 import flock.community.office.monitoring.backend.domain.gcp.CommandPublisher
 import flock.community.office.monitoring.utils.logging.loggerFor
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(path = ["/switch"])
+@RequestMapping(path = ["/switch/{deviceId}"])
 internal class SwitchController(private val commandPublisher: CommandPublisher) {
 
     private val logger = loggerFor<SwitchController>()
 
-    @GetMapping
-    internal fun on() {
+    @PostMapping("/on")
+    internal fun on(@PathVariable deviceId: String) {
         logger.info("Switch on")
-        commandPublisher.on("deviceId")
+        commandPublisher.on(deviceId)
+    }
+
+    @PostMapping("/off")
+    internal fun off(@PathVariable deviceId: String) {
+        logger.info("Switch off")
+        commandPublisher.off(deviceId)
+    }
+
+    @PostMapping("/state")
+    internal fun state(@PathVariable deviceId: String) {
+        logger.info("Switch current state")
+        commandPublisher.state(deviceId)
     }
 }
--- office-backend/src/main/kotlin/flock/community/office/monitoring/backend/domain/gcp/CommandPublisher.kt
+++ office-backend/src/main/kotlin/flock/community/office/monitoring/backend/domain/gcp/CommandPublisher.kt
@@ -3,9 +3,6 @@ package flock.community.office.monitoring.backend.domain.gcp
 import com.google.protobuf.ByteString
 import com.google.pubsub.v1.PubsubMessage
 import flock.community.office.monitoring.utils.logging.loggerFor
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate
 import org.springframework.stereotype.Service
 
@@ -18,9 +15,25 @@ internal class CommandPublisher(
 
     fun on(deviceId: String) {
         // message should be like https://www.zigbee2mqtt.io/devices/SP_120.html
-        val data = ByteString.copyFromUtf8("{\"deviceId\":$deviceId,\"state\":\"ON\"}")
+        val data = ByteString.copyFromUtf8("{\"deviceId\":$deviceId,\"message\":{\"state\":\"ON\"}}")
         val pubsubMessage = PubsubMessage.newBuilder().setData(data).build()
         pubSubTemplate.publish(pubSubConfig.commandTopicName, pubsubMessage)
         logger.info("Changed state for device $deviceId to on")
+    }
+
+    fun off(deviceId: String) {
+        // message should be like https://www.zigbee2mqtt.io/devices/SP_120.html
+        val data = ByteString.copyFromUtf8("{\"deviceId\":$deviceId,\"message\":{\"state\":\"OFF\"}}")
+        val pubsubMessage = PubsubMessage.newBuilder().setData(data).build()
+        pubSubTemplate.publish(pubSubConfig.commandTopicName, pubsubMessage)
+        logger.info("Changed state for device $deviceId to off")
+    }
+
+    fun state(deviceId: String) {
+        // message should be like https://www.zigbee2mqtt.io/devices/SP_120.html
+        val data = ByteString.copyFromUtf8("{\"deviceId\":$deviceId,\"message\":{\"state\":\"\"}}")
+        val pubsubMessage = PubsubMessage.newBuilder().setData(data).build()
+        pubSubTemplate.publish(pubSubConfig.commandTopicName, pubsubMessage)
+        logger.info("Get state for device $deviceId")
     }
 }
