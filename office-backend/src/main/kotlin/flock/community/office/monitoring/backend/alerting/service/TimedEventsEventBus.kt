b63518db571a1659004a264b899b4edf174c669c
Julius van Dis <vandis.j@gmail.com>
2021-06-18 15:12:05 +0200

    Improve concurrency around TimedEventsEventBus

--- office-backend/src/main/kotlin/flock/community/office/monitoring/backend/alerting/service/TimedEventsEventBus.kt
+++ office-backend/src/main/kotlin/flock/community/office/monitoring/backend/alerting/service/TimedEventsEventBus.kt
@@ -2,21 +2,21 @@ package flock.community.office.monitoring.backend.alerting.service
 
 import flock.community.office.monitoring.backend.alerting.domain.TimedUpdate
 import flock.community.office.monitoring.utils.logging.loggerFor
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.asContextElement
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
+import org.springframework.beans.factory.DisposableBean
 import org.springframework.stereotype.Component
 import java.time.Duration
 import java.time.Instant
-import org.springframework.beans.factory.DisposableBean
 import kotlin.math.absoluteValue
 
 data class TimedUpdateRequest(
@@ -24,46 +24,47 @@ data class TimedUpdateRequest(
     val timedUpdate: TimedUpdate
 )
 
+sealed class ScheduledEvents
+class PublishEvent(val timedUpdateRequest: TimedUpdateRequest) : ScheduledEvents()
+class PopEvents(val response: CompletableDeferred<Set<TimedUpdateRequest>>) : ScheduledEvents()
+
 @Component
 class TimedEventsEventBus : DisposableBean {
     private val _events: MutableSharedFlow<TimedUpdate> = MutableSharedFlow(replay = 1)
-    private val coroutineScope = CoroutineScope(CoroutineName("TimedEventsEventBus"))
+    private val scope = CoroutineScope(CoroutineName("TimedEventsEventBus"))
+    private val scheduledEventsActor = scope.scheduledActor()
 
     private val interval = Duration.ofSeconds(10)
     private val leeway = Duration.ofSeconds(5)
-    private val scheduledEvents: MutableSet<TimedUpdateRequest> = mutableSetOf()
 
     private val log = loggerFor<TimedEventsEventBus>()
 
     init {
-        coroutineScope.launch {
+        scope.launch {
             do {
                 log.debug("Processing TimedUpdateRequests ....")
-                scheduledEvents.map {
-                    if ((it.instant.epochSecond - Instant.now().epochSecond).absoluteValue < leeway.toSeconds()) {
-                        val tryEmit = _events.tryEmit(it.timedUpdate)
-                        if (tryEmit) {
-                            scheduledEvents.remove(it)
-                        } else {
-                            log.warn("Processing scheduled event failed. Will try again in $interval. (${it.timedUpdate})")
-                        }
-                    }
+                val response = CompletableDeferred<Set<TimedUpdateRequest>>()
+                scheduledEventsActor.send(PopEvents(response))
+                val timedUpdateRequests: Set<TimedUpdateRequest> = response.await()
+                timedUpdateRequests.forEach {
+                    _events.emit(it.timedUpdate)
                 }
                 log.debug("Done Processing TimedUpdateRequests")
                 delay(interval.toMillis())
             } while (true)
         }
 
-        coroutineScope.launch {
-            delay(15_000L)
-            log.info("Cancelling coroutinescope")
-            coroutineScope.cancel("Cancelling")
-        }
+        // TODO: Remove me after confirming stopping it works
+//        scope.launch {
+//            delay(15_000L)
+//            log.info("Cancelling coroutinescope")
+//            scope.cancel("Cancelling")
+//        }
 
     }
 
-    fun publish(timedUpdateRequest: TimedUpdateRequest): Boolean {
-        scheduledEvents.add(timedUpdateRequest)
+    suspend fun publish(timedUpdateRequest: TimedUpdateRequest): Boolean {
+        scheduledEventsActor.send(PublishEvent(timedUpdateRequest))
         return true;
     }
 
@@ -71,9 +72,30 @@ class TimedEventsEventBus : DisposableBean {
         return _events.asSharedFlow()
     }
 
+    @OptIn(ObsoleteCoroutinesApi::class)
+    private fun CoroutineScope.scheduledActor() = actor<ScheduledEvents> {
+        val scheduledEvents: MutableSet<TimedUpdateRequest> = mutableSetOf()
+
+        for (msg in channel) {
+            when (msg) {
+                is PublishEvent -> scheduledEvents.add(msg.timedUpdateRequest)
+                is PopEvents -> {
+                    val eventsToPopEvents: MutableSet<TimedUpdateRequest> = scheduledEvents.toMutableSet()
+                    eventsToPopEvents.retainAll {
+                        val secondsUntilEvent = (it.instant.epochSecond - Instant.now().epochSecond).absoluteValue
+                        secondsUntilEvent < leeway.toSeconds()
+                    }
+                    msg.response.complete(eventsToPopEvents.toSet())
+                    scheduledEvents.removeAll(eventsToPopEvents)
+                }
+            }
+        }
+
+    }
+
     override fun destroy() {
         log.info("Shutting down '${this::class.simpleName}'")
-        coroutineScope.cancel("Stopping application")
+        scope.cancel("Stopping application")
     }
 
 
