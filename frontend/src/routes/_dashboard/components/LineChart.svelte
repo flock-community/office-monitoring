d6f924c39512e7aee9401e2d05bb8c6719beb15a
Julius van Dis <vandis.j@gmail.com>
2021-09-15 23:03:39 +0200

    Fix chart update issues

--- frontend/src/routes/_dashboard/components/LineChart.svelte
+++ frontend/src/routes/_dashboard/components/LineChart.svelte
@@ -1,67 +1,71 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import * as am4core from "@amcharts/amcharts4/core";
-  import * as am4charts from "@amcharts/amcharts4/charts";
-  import type { Readable } from "svelte/store";
-  import type { LineChartRecord } from "./model";
+    import {onDestroy, onMount} from "svelte";
+    import * as am4core from "@amcharts/amcharts4/core";
+    import * as am4charts from "@amcharts/amcharts4/charts";
+    import type {Readable} from "svelte/store";
+    import type {LineChartRecord} from "./model";
 
-  export let chartRecords: Readable<Partial<LineChartRecord>[]>;
-  export let tempSensorIds: string[];
+    export let chartRecords: Readable<Partial<LineChartRecord>[]>;
+    export let tempSensorIds: string[];
 
-  onMount(async () => {
-    am4core.ready(initChart);
-    chartRecords.subscribe((records) => {
-      chart.data = records;
+    onMount(async () => {
+        am4core.ready(initChart);
+        chartRecords.subscribe((records) => {
+            chart.data = records;
+        });
     });
-  });
 
-  let chart: am4charts.XYChart;
+    onDestroy(() => {
+        chart.dispose()
+    });
 
-  function initChart() {
-    chart = am4core.create("linechart", am4charts.XYChart);
-    chart.logo.disabled = true;
+    let chart: am4charts.XYChart;
 
-    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.grid.template.location = 0;
-    dateAxis.interpolationDuration = 500;
-    dateAxis.rangeChangeDuration = 500;
-    dateAxis.tooltipDateFormat = "HH:mm:ss";
+    function initChart() {
+        chart = am4core.create("linechart", am4charts.XYChart);
+        chart.logo.disabled = true;
 
-    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dateAxis.renderer.grid.template.location = 0;
+        dateAxis.interpolationDuration = 500;
+        dateAxis.rangeChangeDuration = 500;
+        dateAxis.tooltipDateFormat = "HH:mm:ss";
 
-    function addSeries(tempValueField) {
-      const series = chart.series.push(new am4charts.LineSeries());
-      series.dataFields.valueY = tempValueField;
-      series.dataFields.dateX = "date";
-      series.strokeWidth = 2;
-      series.minBulletDistance = 10;
-      series.tooltipText = "{" + tempValueField + "}";
-      series.tooltip.pointerOrientation = "vertical";
-      series.tooltip.background.cornerRadius = 20;
-      series.tooltip.background.fillOpacity = 0.5;
-      series.tooltip.label.padding(12, 12, 12, 12);
-      series.tensionX = 0.8;
-      series.interpolationDuration = 500;
-      series.defaultState.transitionDuration = 0;
+        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-      const bullet = series.createChild(am4charts.CircleBullet);
-      bullet.circle.radius = 5;
-      bullet.fillOpacity = 1;
-      bullet.fill = chart.colors.getIndex(0);
-      bullet.isMeasured = false;
+        function addSeries(tempValueField) {
+            const series = chart.series.push(new am4charts.LineSeries());
+            series.dataFields.valueY = tempValueField;
+            series.dataFields.dateX = "date";
+            series.strokeWidth = 2;
+            series.minBulletDistance = 10;
+            series.tooltipText = "{" + tempValueField + "}";
+            series.tooltip.pointerOrientation = "vertical";
+            series.tooltip.background.cornerRadius = 20;
+            series.tooltip.background.fillOpacity = 0.5;
+            series.tooltip.label.padding(12, 12, 12, 12);
+            series.tensionX = 0.8;
+            series.interpolationDuration = 500;
+            series.defaultState.transitionDuration = 0;
 
-      series.events.on("validated", function () {
-        bullet.moveTo(series.dataItems.last.point);
-        bullet.validatePosition();
-      });
+            const bullet = series.createChild(am4charts.CircleBullet);
+            bullet.circle.radius = 5;
+            bullet.fillOpacity = 1;
+            bullet.fill = chart.colors.getIndex(0);
+            bullet.isMeasured = false;
+
+            series.events.on("validated", function () {
+                bullet.moveTo(series.dataItems.last.point);
+                bullet.validatePosition();
+            });
+        }
+
+        chart.scrollbarX = new am4charts.XYChartScrollbar();
+        chart.cursor = new am4charts.XYCursor();
+        chart.cursor.xAxis = dateAxis;
+
+        tempSensorIds.forEach((id) => addSeries(id));
     }
-
-    chart.scrollbarX = new am4charts.XYChartScrollbar();
-    chart.cursor = new am4charts.XYCursor();
-    chart.cursor.xAxis = dateAxis;
-
-    tempSensorIds.forEach((id) => addSeries(id));
-  }
 </script>
 
-<div class="w-full h-full" id="linechart" />
+<div class="w-full h-full" id="linechart"/>
