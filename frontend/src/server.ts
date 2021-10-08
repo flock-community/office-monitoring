fe75191c847ea9b7f39fd80f8bb5dada597c0d40
Florian Putker <florian.m.putker@gmail.com>
2021-10-08 13:22:47 +0200

    Add to README and switch frontend to express

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -1,17 +1,18 @@
 import sirv from "sirv";
 import polka from "polka";
 import compression from "compression";
+import express from "express";
 import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-polka() // You can also use Express
+express() // You can also use Express
   .use(
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()
   )
-  .listen(PORT, (err: any) => {
-    if (err) console.log("error", err);
+  .listen(PORT, () => {
+    //if (err) console.log("error", err);
   });
--- office-backend/README.md
+++ office-backend/README.md
@@ -8,3 +8,18 @@ To ensure efficient querying, we added composite indexes to the data store in gc
 ```bash
 $ gcloud datastore indexes create office-backend/index.yaml
 ```
+
+## Setup google-cloud for the backend
+
+```
+# install google cloud cli tools
+brew install google-cloud-sdk
+
+# login to get credentials
+gcloud auth login
+
+# Set personal credentials as default application credentials
+gcloud auth application-default login
+```
+
+
